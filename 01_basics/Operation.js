20c1151a783c81611f7b443b26e3b80f6293315f
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2024-10-20 16:51:03 +0000

    Operators

---
+++ 01_basics/Operation.js
@@ -0,0 +1,42 @@
+let value =3;
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1="Hello"
+let str2="Bhawneet"
+let str3=str1+" "+str2
+console.log(str3);
+
+console.log(1+"2");//here 12
+console.log("1"+2);//here 12
+console.log("1"+"2");//here 12
+console.log("1"+2+2);//here 122
+console.log(1+2+"2");//here 32 pehle number to add
+//agar pehle string to concat
+//agar pehle number to add
+
+console.log(+true);//convert to number
+//console.log(true+);//error
+console.log(+"");
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.table([num1,num2,num3]);
+
+
+let gamecounter =100;
+y=gamecounter++;//pehle use phir increment
+console.log(y);//100
+console.log(gamecounter);//101
+
+let z=++gamecounter;
+console.log(z);//pehle increment phir store 102
+console.log(gamecounter);//102
