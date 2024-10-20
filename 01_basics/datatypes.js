2f7d079990978f33e8d348959f26ac5c8c94c92a
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2024-10-20 16:26:10 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,37 @@
+"use strict";//treat all JS code as newer version
+//Primitive
+    //number
+    //string
+    //boolean
+    //NAN
+    //undefined
+    //symbol
+    //null
+
+//Reference
+    //Array
+    //Object
+    //Function
+
+// alert(3+3) //error as in node but it is in browser
+console.log(3+3);
+//code readability should be high
+let name="Bhawneet";
+let age=20;
+let isLoggedIn= false;
+
+//number => 2^53
+//bigint
+//string=>""
+//boolean =>true/false
+//null =>standalone value(khalli hai par undefined nahi hai)
+//undefined =>value not defined
+//symbol =>unique
+
+//object
+
+
+console.log(typeof "Bhawneet");
+console.log(age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
